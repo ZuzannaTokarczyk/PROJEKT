ea7f129f4922805070e0ea663cb309f04949f1e6
unknown <zuzannatokarczyk03@gmail.com>
2023-06-29 21:48:10 +0200

    Zaliczenie projektu na ocenę 3

--- frontend/src/pages/CustomersPage/components/Columns.jsx
+++ frontend/src/pages/CustomersPage/components/Columns.jsx
@@ -27,14 +27,24 @@ export const Columns = [
     enableHiding: false,
   },
   {
-    accessorKey: "fullname",
+    accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Fullname" />
+      <DataTableColumnHeader column={column} title="Name" />
     ),
-    cell: ({ row }) => <div className="w-[80px]">{row.getValue("fullname")}</div>,
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("name")}</div>,
     enableSorting: false,
     enableHiding: false,
   },
+  {
+    accessorKey: "surname",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Surname" />
+    ),
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("surname")}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  
   {
     accessorKey: "email",
     header: ({ column }) => (
@@ -51,14 +61,14 @@ export const Columns = [
     },
   },
   {
-    accessorKey: "phoneNumber",
+    accessorKey: "phone_number",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Phone number" />
     ),
     cell: ({ row }) => {
       return (
         <div className="flex w-[100px] items-center">
-          {row.getValue("phoneNumber")}
+          {row.getValue("phone_number")}
         </div>
       );
     },
